f14b5ff745c1cc55183b7d2793ec14c21b02a541
Emmanuel <emmanuel.cohen00@gmail.com>
2022-09-06 20:35:11 +0200

    add material card for show posts and form for comment

---
+++ src/app/shared/components/comments/comments.component.ts
@@ -0,0 +1,34 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Comment} from '../../../core/models/comment.model'
+import {FormBuilder, FormControl, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.scss']
+})
+export class CommentsComponent implements OnInit {
+
+  @Input() comments!: Comment[]
+  @Output() newComment = new EventEmitter<string>();
+
+  commentControl!: FormControl
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.commentControl = this.formBuilder.control(
+      '',
+      [Validators.required, Validators.minLength(10)]
+    )
+  }
+
+  onLeaveComment() {
+    if (this.commentControl.invalid) {
+      return;
+    }
+    this.newComment.emit(this.commentControl.value);
+    this.commentControl.reset();
+  }
+}
