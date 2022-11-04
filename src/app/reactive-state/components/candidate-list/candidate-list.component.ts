557a047eaa2abf4a4740fa5c340f4e7e9a42d753
Emmanuel <emmanuel.cohen00@gmail.com>
2022-11-04 09:32:34 +0100

    add state management pattern

---
+++ src/app/reactive-state/components/candidate-list/candidate-list.component.ts
@@ -0,0 +1,30 @@
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {CandidatesService} from "../../services/candidates.service";
+import {Candidate} from "../../models/candidate.model";
+
+@Component({
+  selector: 'app-candidate-list',
+  templateUrl: './candidate-list.component.html',
+  styleUrls: ['./candidate-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CandidateListComponent implements OnInit {
+
+  loading$!: Observable<boolean>;
+  candidates$!: Observable<Candidate[]>;
+
+  constructor(private candidatesService: CandidatesService) {
+  }
+
+  ngOnInit(): void {
+    this.initObservable();
+    this.candidatesService.getCandidatesFromServer();
+  }
+
+  private initObservable() {
+    this.loading$ = this.candidatesService.loading$;
+    this.candidates$ = this.candidatesService.candidates$;
+  }
+
+}
